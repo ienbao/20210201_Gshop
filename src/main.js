8e4f14fb4a0083c3ef18a199fdf96af92ead3d13
tianyuanxing <ien_bao@126.com>
2021-01-31 22:21:14 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/*
+  vue 入口
+ */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+const vm = new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
+console.log(vm)
